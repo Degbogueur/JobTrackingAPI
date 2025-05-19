56d79bf4d7ef619d85e3dfff9dcdd4011df319ca
Degbogueur <degbo80@gmail.com>
2025-05-19 11:03:40 -0600

    Complete DashboardService Methods

--- Services/DashboardService.cs
+++ Services/DashboardService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Globalization;
+using Humanizer;
 using JobTrackingAPI.Contracts.Repositories;
 using JobTrackingAPI.Contracts.Results;
 using JobTrackingAPI.Contracts.Services;
@@ -43,9 +44,18 @@ public class DashboardService(
             TotalApplications = await GetTotalApplicationsAsync(),
             ApplicationsInProgress = await GetApplicationsInProgressAsync(),
             ResponseRate = await GetApplicationsResponseRateAsync(),
-            AverageResponseTime = await GetApplicationsAverageResponseTimeAsync()
+            AverageResponseTime = await GetApplicationsAverageResponseTimeAsync(),
+            StatusDistribution = await GetApplicationsStatusDistributionAsync(),
+            MonthlyDistribution = await GetApplicationsMonthlyDistributionAsync(),
+            SourceDistribution = await GetApplicationsSourceDistributionAsync(),
+            ContractTypeDistribution = await GetApplicationsContractTypeDistributionAsync(),
+            PriorityDistribution = await GetApplicationsPriorityDistributionAsync(),
+            TopEnterprises = await GetApplicationsTopEnterprisesAsync(),
+            TopLocations = await GetApplicationsTopLocationsAsync(),
+            NextActions = await GetApplicationsNextActionsAsync(),
+            RecentApplications = await GetRecentApplicationsAsync()
         });
-    }
+    }    
 
     private IQueryable<Application> GetAll()
     {
@@ -94,5 +104,145 @@ public class DashboardService(
         var averageDays = (double)totalDays / count;
 
         return Math.Round(averageDays, 2);
+    }
+
+    private async Task<Dictionary<string, int>> GetApplicationsStatusDistributionAsync()
+    {
+        var applications = GetAll();
+        return await applications
+            .GroupBy(a => a.Status)
+            .Select(a => new
+            {
+                Status = a.Key.Humanize(),
+                Count = a.Count()
+            })
+            .ToDictionaryAsync(
+                x => x.Status,
+                x => x.Count);
+    }
+
+    private async Task<Dictionary<string, int>> GetApplicationsMonthlyDistributionAsync()
+    {
+        var applications = GetAll();
+        return await applications
+            .GroupBy(a => a.ApplicationDate.Month)
+            .Select(a => new
+            {
+                Month = a.Key,
+                Count = a.Count()
+            })
+            .ToDictionaryAsync(
+                x => CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(x.Month),
+                x => x.Count);
+    }
+
+    private async Task<Dictionary<string, int>> GetApplicationsSourceDistributionAsync()
+    {
+        var applications = GetAll();
+        return await applications
+            .GroupBy(a => a.Source)
+            .Select(a => new
+            {
+                Source = a.Key.Humanize(),
+                Count = a.Count()
+            })
+            .ToDictionaryAsync(
+                x => x.Source,
+                x => x.Count);
+    }
+
+    private async Task<Dictionary<string, int>> GetApplicationsContractTypeDistributionAsync()
+    {
+        var applications = GetAll();
+        return await applications
+            .GroupBy(a => a.ContractType)
+            .Select(a => new
+            {
+                ContractType = a.Key.Humanize(),
+                Count = a.Count()
+            })
+            .ToDictionaryAsync(
+                x => x.ContractType,
+                x => x.Count);
+    }
+
+    private async Task<Dictionary<string, int>> GetApplicationsPriorityDistributionAsync()
+    {
+        var applications = GetAll();
+        return await applications
+            .GroupBy(a => a.Priority)
+            .Select(a => new
+            {
+                Priority = a.Key.Humanize(),
+                Count = a.Count()
+            })
+            .ToDictionaryAsync(
+                x => x.Priority,
+                x => x.Count);
+    }
+
+    private async Task<Dictionary<string, int>> GetApplicationsTopEnterprisesAsync()
+    {
+        var applications = GetAll();
+        return await applications
+            .GroupBy(a => a.CompanyName)
+            .Select(a => new
+            {
+                CompanyName = a.Key,
+                Count = a.Count()
+            })
+            .OrderByDescending(x => x.Count)
+            .Take(5)
+            .ToDictionaryAsync(
+                x => x.CompanyName,
+                x => x.Count);
+    }
+
+    private async Task<Dictionary<string, int>> GetApplicationsTopLocationsAsync()
+    {
+        var applications = GetAll();
+        return await applications
+            .GroupBy(a => a.Location)
+            .Select(a => new
+            {
+                Location = a.Key,
+                Count = a.Count()
+            })
+            .OrderByDescending(x => x.Count)
+            .Take(5)
+            .ToDictionaryAsync(
+                x => x.Location,
+                x => x.Count);
+    }
+
+    private async Task<List<NextAction>> GetApplicationsNextActionsAsync()
+    {
+        var applications = GetAll();
+        return await applications
+            .Where(a => a.NextActionDate.HasValue)
+            .OrderByDescending(a => a.NextActionDate!.Value)
+            .Select(a => new NextAction
+            {
+                ActionName = a.NextAction.Humanize(),
+                ActionDate = a.NextActionDate
+            })
+            .ToListAsync();
+    }
+
+    private async Task<List<RecentApplication>> GetRecentApplicationsAsync()
+    {
+        var applications = GetAll();
+        return await applications
+            .OrderByDescending(a => a.ApplicationDate)
+            .Take(5)
+            .Select(a => new RecentApplication
+            {
+                Id = a.Id,
+                ApplicationDateTime = a.ApplicationDate,
+                CompanyName = a.CompanyName,
+                JobTitle = a.JobTitle,
+                Status = a.Status.Humanize()
+            })
+            .ToListAsync();
     }
 }
