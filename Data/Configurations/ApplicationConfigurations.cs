6dade23d51b77d45d4fb94eadb3bede5c4e1387c
Degbogueur <degbo80@gmail.com>
2025-05-19 11:20:21 -0600

    Add Indexes To Applications Table Config and Migrations

---
+++ Data/Configurations/ApplicationConfigurations.cs
@@ -0,0 +1,19 @@
+﻿using JobTrackingAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JobTrackingAPI.Data.Configurations;
+
+public class ApplicationConfigurations : IEntityTypeConfiguration<Application>
+{
+    public void Configure(EntityTypeBuilder<Application> builder)
+    {
+        builder.HasIndex(x => x.CompanyName);
+
+        builder.HasIndex(x => x.Location);
+
+        builder.HasIndex(x => x.JobTitle);
+
+        builder.HasIndex(x => x.ApplicationDate);
+    }
+}
---
+++ Data/Migrations/20250519171732_Add Indexes To Applications Table.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobTrackingAPI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexesToApplicationsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Applications",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "JobTitle",
+                table: "Applications",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyName",
+                table: "Applications",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_ApplicationDate",
+                table: "Applications",
+                column: "ApplicationDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_CompanyName",
+                table: "Applications",
+                column: "CompanyName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_JobTitle",
+                table: "Applications",
+                column: "JobTitle");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Applications_Location",
+                table: "Applications",
+                column: "Location");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Applications_ApplicationDate",
+                table: "Applications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Applications_CompanyName",
+                table: "Applications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Applications_JobTitle",
+                table: "Applications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Applications_Location",
+                table: "Applications");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Location",
+                table: "Applications",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "JobTitle",
+                table: "Applications",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyName",
+                table: "Applications",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
